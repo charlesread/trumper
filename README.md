45dab02ba7461a258904ecbc7d990c3baae1ce7d
Charles Read <physixrox@gmail.com>
2017-04-29 18:12:04 -0400

    initial

---
+++ README.md
@@ -0,0 +1,29 @@
+# About
+
+Who doesn't love Donald Trump?!  Let's make his tweets more accurate by making some corrections!
+
+This little Twitter "bot" finds Trump's latest tweet, changes a few things around, and posts it to the Twitter account of your choice! (currently @realD0naldTroll).
+
+# Installation
+
+`git clone <repo>`
+
+`cd trumper`
+
+`npm install`
+
+`cp config.js.example config.js` (fill out your Twitter app info)
+
+`node index.js`
+
+# Notes
+
+Run `index.js` as much as you like, I have it running every minute via `cron`.
+
+Replacements are made according to the [`Regex`, `String`] arrays in `config.js`.
+
+Have fun!
+
+# Contribution
+
+Fork me to some changes or additions and maybe I'll redeploy the app pointed at @realD0naldTr0ll!
---
+++ index.js
@@ -0,0 +1,64 @@
+'use strict'
+
+require('require-self-ref')
+
+const fs = require('fs')
+const decode = require('decode-html')
+const Twitter = require('twitter')
+
+let config
+
+try {
+  config = require('~/config')
+} catch (err) {
+  throw new Error('cannot find config.js, did you `cp config.js.example config.js`?')
+}
+
+const client = new Twitter(config.twitter)
+
+function recordLastTweetId(tweet) {
+  fs.writeFileSync('./lastTweetId.txt', tweet.id)
+  return Promise.resolve()
+}
+
+function getLastTweetId() {
+  return Promise.resolve(fs.readFileSync('./lastTweetId.txt').toString())
+}
+
+function processTweet(tweet) {
+  let originalText = decode(tweet.full_text)
+  let newText = originalText
+  let numberOfLinks = (newText.match(/http/gi) || []).length
+  //get rid of the last link
+  if (numberOfLinks > 0) {
+    newText = newText.substring(0, newText.lastIndexOf(" "))
+  }
+  //process replacements
+  for (let replacement of config.replacements) {
+    newText = newText.replace(replacement[0], replacement[1])
+  }
+  console.log('O: %s', originalText)
+  console.log('N: %s', newText)
+  return Promise.resolve(newText)
+}
+
+!async function () {
+  //get tweets
+  let tweets = await client.get('statuses/user_timeline', {screen_name: 'realDonaldTrump', tweet_mode: 'extended'})
+  //filter out retweets and get most recent tweet
+  tweets = tweets.filter((tweet) => {
+    return tweet.retweeted_status ? false : true
+  })
+  let currentTweet = tweets[0]
+  let currentTweetId = currentTweet.id
+  let lastTweetId = await getLastTweetId()
+  if (currentTweetId == lastTweetId) {
+    console.log('no new tweets')
+  } else {
+    let text = await processTweet(currentTweet)
+    //note the tweet that was just processed so that it doesn't get processed again
+    recordLastTweetId(currentTweet)
+    await client.post('statuses/update', {status: text})
+  }
+}()
+  .catch(console.error)
